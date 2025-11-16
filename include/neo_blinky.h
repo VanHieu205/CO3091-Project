2d41b363640d6b104de376c149467c6f81bdb723
hoangtranktmt0611 <hoang.tranktmt0611@hcmut.edu.vn>
2025-11-16 18:00:32 +0700

    Update neo_blinky.h

--- include/neo_blinky.h
+++ include/neo_blinky.h
@@ -2,13 +2,16 @@
 #define __NEO_BLINKY__
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-
+#include"global.h"
+#include "monitor_button.h"
 
 
 #define NEO_PIN 45
-#define LED_COUNT 1 
+#define LED_COUNT 1
+
+
 
 void neo_blinky(void *pvParameters);
-
+void neo_blinky_realtime(void *pvParameters);
 
 #endif
