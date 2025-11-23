cbfc03782c17f7c6246c5da30ade27234359b2e1
hoangtranktmt0611 <hoang.tranktmt0611@hcmut.edu.vn>
2025-11-23 15:17:51 +0700

    Update global.cpp

--- src/global.cpp
+++ src/global.cpp
@@ -11,6 +11,9 @@ unsigned long bootMillis = 0;
 unsigned long const LED_TIMEOUT_MS = 10000;
 int led1_mode = LED_AUTO;
 int led2_mode = LED_AUTO;
+int r_glob = 0;
+int g_glob = 0;
+int b_glob = 0;
 
 String ssid = "ESP32-YOUR NETWORK HERE!!!";
 String password = "12345678";
