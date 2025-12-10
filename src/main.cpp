ec704fe238b26411e58a45690c46bf10fe7c4116
hoangtranktmt0611 <hoang.tranktmt0611@hcmut.edu.vn>
2025-12-10 08:52:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,13 @@ extern void data_logger_task(void *pvParameters);
 void setup()
 {
   Serial.begin(115200);
-  //xTaskCreate(led_blinky, "Task LED Blink", 4096, NULL, 2, NULL);
-  //xTaskCreate(neo_blinky, "Task NEO Blink" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 4096, NULL, 2, NULL);
-  //xTaskCreate(task_monitor_button, "Task Monitor Button", 2048, NULL, 2, NULL);
-  //xTaskCreate(main_server_task, "Task Main Server", 8192, NULL, 2, NULL);
-  //xTaskCreate(tiny_ml_task, "Tiny ML Task", 4096, NULL, 2, NULL);
-  //xTaskCreate(evaluation_task, "Evaluation Task" ,4096 ,NULL  ,2 , NULL);
+  //xTaskCreate(led_blinky, "Task LED Blink", 4096, NULL, 10, NULL);
+  //xTaskCreate(neo_blinky, "Task NEO Blink" ,2048  ,NULL  ,10 , NULL);
+  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 4096, NULL, 10, NULL);
+  //xTaskCreate(task_monitor_button, "Task Monitor Button", 2048, NULL, 3, NULL);
+  //xTaskCreate(main_server_task, "Task Main Server", 8192, NULL, 1, NULL);
+  //xTaskCreate(tiny_ml_task, "Tiny ML Task", 4096, NULL, 10, NULL);
+  //xTaskCreate(evaluation_task, "Evaluation Task" ,4096 ,NULL  , 10, NULL);
   xTaskCreate(coreiot_task, "Core Iot Task", 4096, NULL, 2, NULL);
 }
 void loop()
